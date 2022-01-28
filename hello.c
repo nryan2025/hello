d88123bbb070e1b5284923e0385a13f7a3e2e775
Natalie <nryan3@huskers.unl.edu>
2022-01-28 17:08:05 +0000

    added my major to the output

---
+++ hello.c
@@ -0,0 +1,13 @@
+/**
+ * author: Natalie Ryan
+ * date: 2022/01/28
+ * sinmple hello world in C
+ */
+ #include<stdlib.h>
+ #include<stdio.h>
+ int main (int argc, char **argv) {
+     printf("Natalie Ryan Computer Engineering\n");
+     printf("Kianu Benigno Computer Engineering\n");
+
+     return 0;
+ }
